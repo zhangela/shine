432ab181023f6ca8b49536d9839b83a8dcc1b313
Angela Zhang <zhangela@mit.edu>
2014-02-27 13:09:18 -0500

    added prettier log in

--- client/routes.js
+++ client/routes.js
@@ -14,20 +14,33 @@ Router.map(function () {
   });
 
   this.route('assignments', {
-    path: 'assignments/:nameOrWeek/:assignmentType?',
+    path: 'assignments/weeks/:week/:type/questions/add',
     template: 'assignment',
     data: function() {
-      if (! this.params.assignmentType) {
-        return Assignments.findOne({name: this.params.nameOrWeek});
-      } else {
-        return Assignments.findOne({name: this.params.nameOrWeek + this.params.assignmentType});
-      }
+      return Assignments.findOne({week: this.params.week, type: this.params.type});
     }
   });
 
-  this.route('admin/questions/add', {
-    path: '/admin/questions/add',
-    template: 'addQuestion'
+  this.route('addQuestion', {
+    path: 'admin/assignments/weeks/:week/:type/questions/add',
+    template: 'addQuestion',
+    data: function() {
+      return Assignments.findOne({week: this.params.week, type: this.params.type});
+    }
+  });
+
+  this.route('addAssignment', {
+    path: 'admin/assignments/add',
+    template: 'addAssignment',
+    before: function () {
+      if (!Meteor.user()) {
+        // render the login template but keep the url in the browser the same
+        this.render('login');
+
+        // stop the rest of the before hooks and the action function 
+        this.stop();
+      }
+    }
   });
 
   this.route('admin/users/manage', {
