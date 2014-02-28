fd1e14360e80e4f3a1463f834391f9afe1ccb8c1
Sashko Stubailo <sashko@mit.edu>
2014-02-28 17:26:09 -0500

    Removed autopublish and added publishes and subscribes
    We still need real permissions

--- collections/collections.js
+++ collections/collections.js
@@ -2,3 +2,17 @@ Assignments = new Meteor.Collection("assignments");
 Questions = new Meteor.Collection("questions");
 
 UserGroups = new Meteor.Collection("usergroups");
+
+if (Meteor.isServer) {
+  Meteor.publish("assignments", function () {
+    return Assignments.find({});
+  });
+
+  Meteor.publish("questions", function () {
+    return Questions.find({});
+  });
+
+  Meteor.publish("userGroups", function () {
+    return UserGroups.find({});
+  });
+}
