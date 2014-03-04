2cd0cfca43158464c581a4dc2dc12225f0288819
Sashko Stubailo <sashko@mit.edu>
2014-03-03 19:25:12 -0500

    Moved around server stuff

--- collections/collections.js
+++ collections/collections.js
@@ -1,28 +1,3 @@
 Assignments = new Meteor.Collection("assignments");
 Questions = new Meteor.Collection("questions");
-
 UserGroups = new Meteor.Collection("usergroups");
-
-if (Meteor.isServer) {
-  Meteor.publish("assignments", function () {
-    return Assignments.find({});
-  });
-
-  Meteor.publish("questions", function () {
-    return Questions.find({});
-  });
-
-  Meteor.publish("userGroups", function () {
-    if (this.userId) {
-      var currentUser = Meteor.users.find(this.userId);
-
-      if (Permissions.isAdmin(currentUser)) {
-        // admin can see all user groups
-        return UserGroups.find({});
-      } else {
-        // owners can see their groups
-        return UserGroups.find({owner: this.userId});
-      }
-    }
-  });
-}
